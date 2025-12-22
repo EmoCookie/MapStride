5df6c73c47eb89b581ad91d2899c992640a9b4c7
Fathima <fathima.20200693@iit.ac.lk>
2025-12-22 13:37:24 +0530

    Storing users in session

--- frontend/src/pages/userAuthForm.page.jsx
+++ frontend/src/pages/userAuthForm.page.jsx
@@ -1,18 +1,81 @@
+import { useRef } from "react";
 import AnimationWrapper from "../common/page-animation";
 import InputBox from "../components/input.component";
 import googleIcon from "../imgs/google.png";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
+import { Toaster, toast } from "react-hot-toast";
+import axios from "axios";
+import { storeInSession } from "../common/session";
+import { useContext } from "react";
+import { UserContext } from "../App";
 
 const UserAuthForm = ({ type }) => {
 
+    const authForm = useRef();
+
+    let { userAuth: { access_token }, setUserAuth } = useContext(UserContext)
+
+    const userAuthThroughServer = (serverRoute, formData) => {
+
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, formData)
+        .then(({ data }) => {
+            storeInSession("user", JSON.stringify(data))
+            setUserAuth(data)
+        })
+        .catch(({ response }) => {
+            toast.error(response.data.error);
+        })
+
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        let serverRoute = type == "sign-in" ? "/signin" : "/signup";
+
+        let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+        let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+
+        // form data
+        let form = new FormData(formElement);
+        let formData = {};
+
+        for(let [key, value] of form.entries()){
+            formData[key] = value;
+        }
+
+        let { fullname, email, password } = formData
+
+        // form validation
+        if(fullname){
+            if(fullname.length < 3){
+                return toast.error("Fullname must be at least over three letters.")
+            }
+        }
+
+        if(!email.length){
+            return toast.error("Please enter your email.")
+        }
+
+        if(!emailRegex.test(email)){
+            return toast.error("Invalid email.")
+        }
+
+        if(!passwordRegex.test(password)){
+            return toast.error("The password should have 6 to 20 characters with a number, one lowercase and one uppercase letter.")
+        }
+
+        userAuthThroughServer(serverRoute, formData)
     }
 
     return (
+        access_token ? 
+        <Navigate to="/" />
+        :
         <AnimationWrapper keyValue={type}>
             <section className="h-cover flex items-center justify-center">
-                <form className="w-[80%] max-w-[400px]">
+                <Toaster />
+                <form id="formElement" className="w-[80%] max-w-[400px]">
                     <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
                         {type == "sign-in" ? "Welcome Back!" : "Create an Account"}
                     </h1>

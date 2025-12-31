ca6567a1a4944a622c2cf0939fd783df74beb9f9
EmoCookie <129533088+EmoCookie@users.noreply.github.com>
2025-12-31 20:44:15 +0530

    Remove Firebase config details for security
    Removed sensitive Firebase configuration details.

--- frontend/src/common/firebase.jsx
+++ frontend/src/common/firebase.jsx
@@ -4,12 +4,12 @@ import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 
 // web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCv-CfJrkaxXmckwJdH8vdJJKZcyatn578",
-  authDomain: "mapstride-c93ba.firebaseapp.com",
-  projectId: "mapstride-c93ba",
-  storageBucket: "mapstride-c93ba.firebasestorage.app",
-  messagingSenderId: "1094190359268",
-  appId: "1:1094190359268:web:52c6a6cd6310d6a86f02f1"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
